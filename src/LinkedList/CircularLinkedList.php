317d0ffe11b6c11f29caa7822415c6ce65225825
Siddhant Thapa <siddhantthp@gmail.com>
2019-09-02 08:02:19 +0545

    circular linked list has been implemented.

---
+++ src/LinkedList/CircularLinkedList.php
@@ -0,0 +1,52 @@
+<?php
+    class ListNode {
+        public $data = NULL;
+        public $next = NULL;
+
+        public function __construct(string $data = NULL) {
+            $this->data = $data;
+        }
+    }
+
+    class CircularLinkedList {
+        private $_firstNode = NULL;
+        private $_totalNode = 0;
+
+        public function insertAtEnd(string $data = NULL) {
+            $newNode = new ListNode($data);
+            if ($this->_firstNode === NULL) {
+                $this->_firstNode = &$newNode;
+            } else {
+                $currentNode = $this->_firstNode;
+                while ($currentNode->next !== $this->_firstNode) {
+                    $currentNode = $currentNode->next;
+                }
+                $currentNode->next = $newNode;
+            }
+            $newNode->next = $this->_firstNode;
+            $this->_totalNode++;
+            return TRUE;
+        }
+
+        public function display() {
+            echo "Total book titles: ".$this->_totalNode."\n";
+            $currentNode = $this->_firstNode;
+            while ($currentNode->next !== $this->_firstNode) {
+                echo $currentNode->data."\n";
+                $currentNode = $currentNode->next;
+            }
+
+            if ($currentNode) {
+                echo $currentNode->data."\n";
+            }
+        }
+    }
+
+    $BookTitles = new CircularLinkedList();
+    $BookTitles->insertAtEnd("Introduction to Algorithm");
+    $BookTitles->insertAtEnd("Introduction to PHP and Data Structures");
+    $BookTitles->insertAtEnd("Programming Intelligence");
+    $BookTitles->insertAtEnd("Mediawiki Administrative tutorial guide");
+    $BookTitles->insertAtEnd("Introduction to Calculus");
+    $BookTitles->display();
+?>
